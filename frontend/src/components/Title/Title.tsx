46397747a7c2b360a41c7cfe230fc0c10670abe3
Melichka <melichkaowen1@gmail.com>
2024-01-10 08:34:25 +0300

    feat (front) : add log in form

--- frontend/src/components/Title/Title.tsx
+++ frontend/src/components/Title/Title.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+import { useHomeNavigation } from "../../hooks/useHomeNavigation";
+
 import { BOTTOM_TITLE, UPPER_TITLE } from "./constants";
 
 import styles from "./styles.module.scss";
 
 export const Title = () => {
+  const onHomeNavigation = useHomeNavigation();
+
   return (
     <div className={styles.title}>
       <h1 className={styles.upper}> {UPPER_TITLE} </h1>
