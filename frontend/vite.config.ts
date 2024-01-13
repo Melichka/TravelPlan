38a9949f705c8e438d580c8c88d02823ad42ef30
Marat Zimnurov <zimtir@mail.ru>
2024-01-14 01:08:42 +0300

    build(vite): extend aliases to support src dir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '/@/components': path.resolve(__dirname, 'src/components'),
+      'src': resolve(__dirname, 'src'),
     },
   },
 
