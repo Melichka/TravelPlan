86388fb36b7a7f6acf0ae4a6ecb87a17ee3c0e4e
Melichka <melichkaowen1@gmail.com>
2024-01-17 09:21:38 +0300

    feat(style, frontend): add login and password fields

---
+++ frontend/src/components/InputField/utils.ts
@@ -0,0 +1,13 @@
+export const InputFieldStyles = () => {
+  return {
+    "& .MuiOutlinedInput-root": {
+      borderRadius: 30,
+    },
+  };
+};
+
+export const IconStyles = () => {
+  return {
+    color: "white",
+  };
+};
--- frontend/src/components/Password/Password.tsx
+++ frontend/src/components/Password/Password.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { InputField } from "src/components/InputField";
 import { PASSWORD } from "./constants";
 
-import "./styles.scss";
+import styles from"./styles.module.scss";
 
 export const Password = () => {
   return <InputField placeholder={PASSWORD} isPassword></InputField>;
