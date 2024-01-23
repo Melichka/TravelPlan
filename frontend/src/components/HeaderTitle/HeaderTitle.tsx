f5e446bbd61c59cbd265e20a9a4608916ce2be8f
Melichka <melichkaowen1@gmail.com>
2024-01-23 14:28:43 +0300

    feat(frontend): add navigate to main page

--- frontend/src/components/HeaderTitle/HeaderTitle.tsx
+++ frontend/src/components/HeaderTitle/HeaderTitle.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { BOTTOM_TITLE, UPPER_TITLE } from "./constants";
 
@@ -6,9 +7,10 @@ import styles from "./styles.module.scss";
 
 export const HeaderTitle = () => {
   return (
-    <div className={styles.header}>
-      <h2 className={styles.upper}> {UPPER_TITLE} </h2>
-      <h2 className={styles.bottom}> {BOTTOM_TITLE} </h2>
-    </div>
+    <Link to="/" className={styles.header} >
+        <h2 className={styles.upper}> {UPPER_TITLE} </h2>
+        <h2 className={styles.bottom}> {BOTTOM_TITLE} </h2>
+
+    </Link>
   );
 };
