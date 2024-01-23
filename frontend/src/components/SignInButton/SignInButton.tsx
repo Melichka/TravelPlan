4b01ecf5d41a86ebb7fb52bedf04b22f7087db3e
Melichka <melichkaowen1@gmail.com>
2024-01-23 14:29:09 +0300

    feat(frontend): add navigate to sign in page

--- frontend/src/components/SignInButton/SignInButton.tsx
+++ frontend/src/components/SignInButton/SignInButton.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 import { NavigateButton } from "../NavigateButton";
 
 import { SIGN_IN } from "./constants";
 
+import styles from "./styles.module.scss"
+
 export const SignInButton = () => {
-  return <NavigateButton text={SIGN_IN} isMain />;
+  return (
+    <Link to="/sign-in" className={styles.button} >
+      <NavigateButton text={SIGN_IN} isMain />
+    </Link>
+  );
 };
