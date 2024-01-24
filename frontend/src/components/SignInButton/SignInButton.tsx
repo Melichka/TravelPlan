266c127fe8572505163b404b45efc744cfe6c911
Melichka <melichkaowen1@gmail.com>
2024-01-24 13:27:33 +0300

    feat(frontend): add translation

--- frontend/src/components/SignInButton/SignInButton.tsx
+++ frontend/src/components/SignInButton/SignInButton.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 import { NavigateButton } from "../NavigateButton";
 
 import { SIGN_IN } from "./constants";
 
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 
 export const SignInButton = () => {
+  const { t } = useTranslation();
   return (
-    <Link to="/sign-in" className={styles.button} >
-      <NavigateButton text={SIGN_IN} isMain />
+    <Link to="/sign-in" className={styles.button}>
+      <NavigateButton text={t("SignInButton")} isMain />
     </Link>
   );
 };
