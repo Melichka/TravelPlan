cb4a2570e3700b6a443cb711a62a1b66a3274a6f
Melichka <melichkaowen1@gmail.com>
2024-01-24 19:52:11 +0300

    feat(frontend): include localization module

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import i18n from "./i18n";
 
 import App from "./App";
 
 import "./index.scss";
 
 import "./i18n";
+import { I18nextProvider } from "react-i18next";
 
 const rootContainer = document.getElementById("root");
 if (rootContainer) {
   const root = ReactDOM.createRoot(rootContainer);
   root.render(
     <React.StrictMode>
-      <App />
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
     </React.StrictMode>
   );
 }
