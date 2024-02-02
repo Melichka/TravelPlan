6a79a04bcd7e7ccf58a5724222b020dddd699926
Melichka <melichkaowen1@gmail.com>
2024-02-02 14:03:28 +0300

    refactor(backend): delete unnecessary file

--- backend/src/database/database.providers.ts
+++
@@ -1,9 +0,0 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { Entertainment } from 'src/entities/entertainment';
-
-export const databaseProviders: TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: 'db/abobadb',
-  entities: [Entertainment],
-  synchronize: true,
-};
