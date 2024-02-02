c2349fc31cefb2a5f5eca69af0f5d76abbeea7ca
Melichka <melichkaowen1@gmail.com>
2024-02-02 10:23:41 +0300

    feat(backend): add entities to ormconfig

--- backend/src/ormconfig.ts
+++ backend/src/ormconfig.ts
@@ -1,9 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { City } from './entities/city';
+import { Entertainment } from './entities/entertainment';
+import { Hotel } from './entities/hotel';
+import { Sight } from './entities/sight';
+import { Tag } from './entities/tag';
+import { User } from './entities/user';
 
 export const ormConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: 'sql.sqlite',
   synchronize: true,
-  entities: [City],
+  entities: [City, User, Entertainment, Sight, Hotel, Tag],
 };
