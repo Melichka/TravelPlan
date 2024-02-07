631ce92614e03c91ef11a0836f47dc2a5e8b8dd3
Melichka <melichkaowen1@gmail.com>
2024-02-07 11:08:55 +0300

    feat(backend): add CRUD for hotel

---
+++ backend/src/hotel/hotel.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Hotel } from 'src/entities/Hotel';
+
+@Injectable()
+export class HotelService {
+  constructor(
+    @InjectRepository(Hotel)
+    private hotelRepository: Repository<Hotel>,
+  ) {}
+
+  async findAll(): Promise<Hotel[]> {
+    return this.hotelRepository.find();
+  }
+
+  async findOne(id: number): Promise<Hotel> {
+    return this.hotelRepository.findOne({ where: { id } });
+  }
+
+  async create(hotel: Partial<Hotel>): Promise<Hotel> {
+    const newhotel = this.hotelRepository.create(hotel);
+    return this.hotelRepository.save(newhotel);
+  }
+
+  async update(id: number, hotel: Partial<Hotel>): Promise<Hotel> {
+    await this.hotelRepository.update(id, hotel);
+    return this.hotelRepository.findOne({ where: { id } });
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.hotelRepository.delete(id);
+  }
+}
