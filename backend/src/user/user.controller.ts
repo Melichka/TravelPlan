8cc704bd86ea4ca8f1edf1c32c4a8aa75a561231
Melichka <melichkaowen1@gmail.com>
2024-02-07 11:08:36 +0300

    refactor(backend): delete comments

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -16,13 +16,11 @@ import { User } from 'src/entities/User';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  //get all users
   @Get()
   async findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
-  //get user by id
   @Get(':id')
   async findOne(@Param('id') id: number): Promise<User> {
     const user = await this.userService.findOne(id);
@@ -33,22 +31,18 @@ export class UserController {
     }
   }
 
-  //create user
   @Post()
   async create(@Body() user: User): Promise<User> {
     return this.userService.create(user);
   }
 
-  //update user
   @Put(':id')
   async update(@Param('id') id: number, @Body() user: User): Promise<any> {
     return this.userService.update(id, user);
   }
 
-  //delete user
   @Delete(':id')
   async delete(@Param('id') id: number): Promise<any> {
-    //handle error if user does not exist
     const user = await this.userService.findOne(id);
     if (!user) {
       throw new NotFoundException('User does not exist!');
