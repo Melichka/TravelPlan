94639362b994d6095482cdd4ed3d34b8c3b377af
Melichka <melichkaowen1@gmail.com>
2024-02-07 10:59:00 +0300

    feat(backend): add CRUD for sight

--- backend/src/sight/sight.module.ts
+++ backend/src/sight/sight.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Sight } from '../entities/Sight';
+
+import { Sight } from 'src/entities/Sight';
+import { SightController } from './sight.controller';
+import { SightService } from './sight.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Sight])],
-  // controllers: [MenuController],
-  // providers: [MenuService],
+  controllers: [SightController],
+  providers: [SightService],
 })
 export class SightModule {}
